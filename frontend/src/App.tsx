895fc664996223f6745b29786ace54e1c3a06b5f
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-10-20 12:23:37 +0300

    updated gitignore

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.scss'
 import Login from './Pages/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
     <div className='App'>
