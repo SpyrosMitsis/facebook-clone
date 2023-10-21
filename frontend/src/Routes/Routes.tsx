17d530c0a431b7bb7d7371548481fb9363543a4b
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-10-21 15:14:05 +0300

    Fixed loginPage scss

--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -9,7 +9,7 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: '',
-                element: <Login />
+                element: <Login />,
             },
         ],
     },
