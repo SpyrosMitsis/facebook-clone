0409d7ccdd8934e2aea479fa8d00301d89432ccf
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-10-25 22:23:18 +0300

    node Js stuff

--- frontend/src/Pages/Home/Home.tsx
+++ frontend/src/Pages/Home/Home.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Header, SideBar, Feed, Widget } from '../../components';
 
-type Props = {}
+import './Home.scss';
 
-const Home = (props: Props) => {
+function Home(): React.ReactElement {
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    (
+      async() => {
+
+        const response = await fetch("http://localhost:5112/api/user", {
+            headers: { 'Content-Type': 'application/json' },
+            credentials: 'include',
+        })
+
+        const content = await response.json()
+
+        setUserName(content.name)
+      }
+    )();
+  });
+
   return (
-    <div>Home</div>
-  )
+    <>
+      <div className='homeBody'>
+        <Feed username={userName}/>
+        <Widget />
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
--- frontend/src/Pages/Login/Login.tsx
+++ frontend/src/Pages/Login/Login.tsx
@@ -3,16 +3,19 @@ import { Button, TextField } from '@material-ui/core';
 import './Login.scss';
 import fbNameLogo from '../../assets/fbNameLogo.png';
 import SignUp from '../../components/SignUp';
+import { useNavigate } from 'react-router-dom';
 
 
 function Login(): React.ReactElement {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [showSignUp, setShowSignUp] = useState(false); 
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [showSignUp, setShowSignUp] = useState(false);
+    const [redirect, setRedirect] = useState(false)
+    const navigate = useNavigate();
 
-  const onSignIn = async (e: SyntheticEvent) => {
-    // You can access the email and password values here and perform the login logic
-    e.preventDefault()
+    const onSignIn = async (e: SyntheticEvent) => {
+        // You can access the email and password values here and perform the login logic
+        e.preventDefault()
 
         await fetch("http://localhost:5112/api/login", {
             method: 'POST',
@@ -23,43 +26,48 @@ function Login(): React.ReactElement {
                 password,
             })
         })
-  };
+        setRedirect(true)
+
+    };
+        if(redirect ){
+            navigate("/home")
+        }
 
     return (
         <>
             <div className='login'>
-                <SignUp show={showSignUp} setShow={setShowSignUp}/>
-            <div className='logo'>
-                <img src={fbNameLogo} alt='name logo' />
-                <h3>shit is hard.</h3>
-            </div>
-            <div className='loginCard'>
-                <div className='card'>
-                <span className='title'>Sign In</span>
-                <p>Sign in with your account or Sign up</p>
-                <TextField className="loginField" 
-                    label="Email"
-                    variant="outlined"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <TextField className="loginField" 
-                    label="Password"
-                    type="password"
-                    variant="outlined"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <Button className='signIn' type='submit' onClick={onSignIn}>
-                   SIGN IN 
-                </Button>
-                <p className='txt-forgot-pass'>Forgotten password?</p>
-                <div className="login-seperator"></div>
-                <Button className='signUp' type='submit' onClick={() => setShowSignUp(true)}>
-                    SIGN UP 
-                </Button>
+                <SignUp show={showSignUp} setShow={setShowSignUp} />
+                <div className='logo'>
+                    <img src={fbNameLogo} alt='name logo' />
+                    <h3>shit is hard.</h3>
                 </div>
-            </div>
+                <div className='loginCard'>
+                    <div className='card'>
+                        <span className='title'>Sign In</span>
+                        <p>Sign in with your account or Sign up</p>
+                        <TextField className="loginField"
+                            label="Email"
+                            variant="outlined"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                        <TextField className="loginField"
+                            label="Password"
+                            type="password"
+                            variant="outlined"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <Button className='signIn' type='submit' onClick={onSignIn}>
+                            SIGN IN
+                        </Button>
+                        <p className='txt-forgot-pass'>Forgotten password?</p>
+                        <div className="login-seperator"></div>
+                        <Button className='signUp' type='submit' onClick={() => setShowSignUp(true)}>
+                            SIGN UP
+                        </Button>
+                    </div>
+                </div>
             </div>
         </>
     );
