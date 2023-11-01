97c051fee3c6d989e30ccb4afb81decc7c8ed3f2
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-11-01 22:52:40 +0200

    chores

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,46 @@
-import { Outlet } from 'react-router'
+import { Outlet, Route, Routes } from 'react-router'
+import React from 'react';
 import './App.scss'
-import { AuthProvider } from './Context/AuthProvider';
+import { AuthProvider, RequireAuth } from 'react-auth-kit'
+import axios from './api/axios';
+import { BrowserRouter } from 'react-router-dom';
+import Login from './Pages/Login/Login';
 
 function App() {
+
+
   return (
     <>
-      <AuthProvider>
-        <main className='App'>
-          <Outlet />
-        </main>
-      </AuthProvider>
+        <Routes>
+        <Route path='/login' element={ <Login />} />
+
+          <Route path={'/secure'} element={
+            <RequireAuth loginPath={'/'}>
+              <div>
+                Secure
+              </div>
+            </RequireAuth>
+          } />
+        </Routes>
     </>
-  );
+
+  )
+
 }
+
+
+ // return (
+ //   <>
+ //     <AuthProvider authType={'cookie'}
+ //       authName={'_auth'}
+ //       cookieDomain={window.location.hostname}
+ //       cookieSecure={false}>
+ //       <main className='App'>
+ //         <Outlet />
+ //       </main>
+ //     </AuthProvider>
+ //   </>
+ // );
+
 
 export default App;
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -1,26 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes, createBrowserRouter } from "react-router-dom";
 import { Login } from "../Pages";
 import App from "../App";
 import Home from "../Pages/Home/Home";
 import Post from "../components/Feed";
+import { RequireAuth } from 'react-auth-kit'
 
-export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-        children: [
-            {
-                path: '',
-                element: <Login />,
-            },
-            {
-                path: '/Home',
-                element: <Home/>,
-            },
-            {
-                path: '/post',
-                element: <Post/>,
-            },
-        ],
-    },
-]);
+
+function App() {
+  return (
+    <BrowserRouter basename="/app">
+      <Routes>
+        <Route path="/" /> {<Login />}
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+
+//export const router = createBrowserRouter([
+//    {
+//        path: '/',
+//        element: <App />,
+//        children: [
+//            {
+//                path: '',
+//                element: <Login />,
+//            },
+//            {
+//                path: '/Home',
+//                element: <Home name={""}/>,
+//            },
+//            {
+//                path: '/post',
+//                element: <Post/>,
+//            },
+//        ],
+//    },
+//]);
