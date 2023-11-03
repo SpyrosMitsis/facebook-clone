7db8098ecbf8bec671baa7f0a12afe67ebb0422c
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-11-03 18:42:04 +0200

    Fixed Upload

--- backend/Dtos/UploadImageDto.cs
+++ backend/Dtos/UploadImageDto.cs
@@ -5,6 +5,6 @@ namespace backend.Dtos
     public class UploadImageDto
     {
         [NotMapped]
-        public IFormFile? ImageFile{ get; set; }
+        public IFormFile? ProfilePic { get; set; }
     }
 }
