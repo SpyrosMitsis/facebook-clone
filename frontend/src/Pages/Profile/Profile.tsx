db395e490de331e0f7b6feecf7d278ac85b748b2
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-11-06 23:31:58 +0200

    Finalized Mock ProfilePage

--- frontend/src/Pages/Profile/Profile.tsx
+++ frontend/src/Pages/Profile/Profile.tsx
@@ -28,17 +28,6 @@ export const FacebookProfile = ({ photoUrl }: Props): JSX.Element => {
     );
 };
 
-export const FacebookImagePost = (): JSX.Element => {
-    return <div className={`facebook-image-post}`} />;
-};
-export const DivWrapper = (): JSX.Element => {
-    return <div className={`div-wrapper`} />;
-};
-
-export const FacebookImagePostWrapper = (): JSX.Element => {
-    return <div className={`facebook-image-post-wrapper`} />;
-};
-
 
 
 
@@ -50,157 +39,79 @@ export const Profile = (): JSX.Element => {
     return (
         <>
             <Header photoUrl={photo} username={profileName} />
-            <div className="mockup-facebook">
-                <div className="div">
-                    <div className="frame">
-                        <div className="cover-picture-wrapper">
-                            <CoverPicture photoUrl={photoUrl} />
+            <div className="frame">
+                <CoverPicture photoUrl={photoUrl} />
+                <div className="frame-3">
+                    <img src={photo} className="profileAvatar" />
+                    <div className="ProfileName"> {profileName}</div>
+                    <div className="FriendsNumber">1K friends</div>
+                    <Button className="edit_profile">
+                        edit profile
+                    </Button>
+                    <Button className="add_story">
+                        Add to story
+                    </Button>
+                </div>
+            </div>
+            <div className="main-content">
+                <div className="frame-15">
+                    <div className="Intro">Intro</div>
+                    <Button className="button">
+                        Add bio
+                    </Button>
+
+                    <Button className="button">
+                        Edit details
+                    </Button>
+
+                    <Button className="button">
+                        Eat Ass
+                    </Button>
+
+                    <Button className="button">
+                        Add hobbies
+                    </Button>
+
+                    <Button className="button">
+                        Add featured
+                    </Button>
+                </div>
+                <div className="Posts">
+                    <div className="card">
+                        <div>
+                            Profile Picture
                         </div>
-                        <div className="frame-2">
-                            <div className="frame-3">
-                                <div className="group">
-                                    <img src={photo} className="profileAvatar" />
-                                </div>
-                                <div className="frame-4">
-                                    <div className="frame-5">
-                                        <div className="ProfileName"> {profileName}</div>
-                                        <div className="FriendsNumber">1K friends</div>
-                                    </div>
-                                    <div className="frame-6">
-                                        <div className="frame-7">
-                                            <img className="img" alt="Icon facebook edit" src="edit-24.svg" />
-                                            <div className="text-wrapper-3">Edit profile</div>
-                                        </div>
-                                        <div className="frame-8">
-                                            <img className="img" alt="Icon facebook edit" src="image.svg" />
-                                            <div className="text-wrapper-4">Add to story</div>
-                                        </div>
-                                        <img className="img-2" alt="Frame" src="frame-44.svg" />
-                                    </div>
-                                </div>
+                        <div className='searchInput1'>
+                            <SearchIcon className='searchIcon1' />
+                            <input type='text' placeholder="What's on your mind?" className='inputBar1' />
+                        </div>
+                        <div className="frame-31">
+                            <div className="frame-32">
+                                <div className="rectangle-3" />
+                                <div className="text-wrapper-14">Live video</div>
                             </div>
-                            <div className="frame-9">
-                                <div className="frame-10">
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-5">Posts</div>
-                                        <div className="rectangle" />
-                                    </div>
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-6">About</div>
-                                    </div>
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-6">Friends</div>
-                                    </div>
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-6">Photos</div>
-                                    </div>
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-6">Videos</div>
-                                    </div>
-                                    <div className="frame-11">
-                                        <div className="text-wrapper-6">Check-ins</div>
-                                    </div>
-                                    <div className="frame-12">
-                                        <div className="text-wrapper-6">More</div>
-                                        <img className="frame-13" alt="Frame" src="frame-51.svg" />
-                                    </div>
-                                </div>
+                            <div className="frame-32">
+                                <div className="rectangle-3" />
+                                <div className="text-wrapper-14">Photo/video</div>
+                            </div>
+                            <div className="frame-32">
+                                <div className="rectangle-3" />
+                                <div className="text-wrapper-14">Feeling/activity</div>
                             </div>
                         </div>
                     </div>
-                    <div className="frame-14">
-                        <div className="frame-15">
-                            <div className="Intro">Intro</div>
-                            <Button className="button">
-                                Add bio
-                            </Button>
-
-                            <Button className="button">
-                                Edit details
-                            </Button>
-
-                            <Button className="button">
-                                Eat Ass
-                            </Button>
-
-                            <Button className="button">
-                                Add hobbies
-                            </Button>
-
-                            <Button className="button">
-                                Add featured
-                            </Button>
-                        </div>
-                        <div className="frame-15">
-                            <div className="frame-22">
-                                <div className="Intro">Photos</div>
-                                <div className="SeeMore">See all photos</div>
-                            </div>
-                            <div className="frame-23">
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                                <div className="placeholderImage" />
-                            </div>
-                        </div>
-                        <div className="frame-25">
-                            <div className="frame-26">
-                                <div className="frame-22">
-                                    <div className="Intro">Friends</div>
-                                    <div className="SeeMore">See all friends</div>
-                                </div>
-                                <div className="FriendsNumber">1 friend</div>
-                            </div>
-                            <div className="frame-27">
-                                <div className="facebook-profile-wrapper">
-                                    <FacebookProfile photoUrl={photo} />
-                                </div>
-                                <div className="UserName ">{currentUser()?.firstName} {currentUser()?.surname}</div>
-                            </div>
-                        </div>
+                <div className="frame-33">
+                    <div className="text-wrapper-15">Posts</div>
+                    <div className="frame-34">
+                        <img className="img" alt="Frame" src="frame-88.svg" />
+                        <div className="text-wrapper-16">Filters</div>
                     </div>
-                    <div className="Posts">
-                        <div className="card">
-                                <div>
-                                    Profile Picture
-                                </div>
-                                <div className='searchInput'>
-                                    <SearchIcon className='searchIcon' />
-                                    <input type='text' placeholder="What's on your mind?" className='inputBar' />
-                                </div>
-                            <div className="frame-31">
-                                <div className="frame-32">
-                                    <div className="rectangle-3" />
-                                    <div className="text-wrapper-14">Live video</div>
-                                </div>
-                                <div className="frame-32">
-                                    <div className="rectangle-3" />
-                                    <div className="text-wrapper-14">Photo/video</div>
-                                </div>
-                                <div className="frame-32">
-                                    <div className="rectangle-3" />
-                                    <div className="text-wrapper-14">Feeling/activity</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="frame-33">
-                            <div className="text-wrapper-15">Posts</div>
-                            <div className="frame-34">
-                                <img className="img" alt="Frame" src="frame-88.svg" />
-                                <div className="text-wrapper-16">Filters</div>
-                            </div>
-                        </div>
-                                <div className="frame-42">
-                                    <div className="text-wrapper-21">Write a comment...</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                </div>
+                </div>
+            </div>
         </>
     );
-};
+}
+;
 
 export default Profile;
