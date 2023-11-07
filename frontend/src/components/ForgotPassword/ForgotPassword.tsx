1781c2b22c65152f0bcf3b45d54dbd694a7efb72
Spyros Mitsis <66162195+SpyrosMitsis@users.noreply.github.com>
2023-11-07 12:54:01 +0200

    Changed to axios patch request

--- frontend/src/components/ForgotPassword/ForgotPassword.tsx
+++ frontend/src/components/ForgotPassword/ForgotPassword.tsx
@@ -2,6 +2,7 @@ import Button from '@mui/material/Button';
 import React, { SyntheticEvent, useState } from 'react'
 import closeIcon from "../../assets/close-icon.png"
 import './ForgotPassword.scss'
+import axios from '../../api/axios';
 
 
 type Props = {
@@ -9,6 +10,7 @@ type Props = {
     setShow: (show: boolean) => void
 }
 
+const LOGIN_URL = '/forgotPassword'
 
 const ForgotPassword = (props: Props) => {
 
@@ -17,44 +19,45 @@ const ForgotPassword = (props: Props) => {
     const [pass2, setPass2] = useState('');
 
     const submit = async (e: SyntheticEvent) => {
-        const content = await fetch("http://localhost:5112/api/forgotPassword", {
-            method: "POST",
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                email,
-                pass1,
-                pass2
-        })
-    })
-}
+        try {
+            const response = await axios.patch(LOGIN_URL, {
+                email: email,
+                password1: pass1,
+                password2: pass2
+            });
+            const content = response.data;
+            console.log(response)
+        } catch (error) {
 
+            console.error("There was an error!", error);
+        }
+    }
 
-
-        return (
-            <div style={{
-                display: props.show ? "flex" : "none"
-            }}>
-                <div className="overlay-signup"></div>
-                <div className="signup">
-                    <div className="signup-container">
-                        <div className="signup-header">
-                            <div>
-                                <h1>Forgot Password?</h1>
-                                <p>No problem.</p>
-                            </div>
-                            <img src={closeIcon} alt="close icon" id="close-icon"
-                                onClick={() => props.setShow(false)}
-       
-                            />
+    return (
+        <div style={{
+            display: props.show ? "flex" : "none"
+        }}>
+            <div className="overlay-signup"></div>
+            <div className="signup">
+                <div className="signup-container">
+                    <div className="signup-header">
+                        <div>
+                            <h1>Forgot Password?</h1>
+                            <p>No problem.</p>
                         </div>
-       
-                        <div className="signup-seperator"></div>
+                        <img src={closeIcon} alt="close icon" id="close-icon"
+                            onClick={() => props.setShow(false)}
 
-                        <form onSubmit={submit}>
-                            <div className="signup-fields">
+                        />
+                    </div>
 
-                                <div className="signup-inputs">
-                                    <div>
+                    <div className="signup-seperator"></div>
+
+                    <form onSubmit={submit}>
+                        <div className="signup-fields">
+
+                            <div className="signup-inputs">
+                                <div>
                                     <input className="default-input" type="text" placeholder='Enter email address' required
                                         onChange={e => setEmail(e.target.value)}
                                     />
@@ -66,17 +69,17 @@ const ForgotPassword = (props: Props) => {
                                     />
                                 </div>
 
-                                </div>
-                                <Button variant='outlined' className='reset-btn' type='submit'>
-                                    Reset Password
-                                </Button>
-
                             </div>
-                        </form>
-                    </div>
+                            <Button variant='outlined' className='reset-btn' type='submit'>
+                                Reset Password
+                            </Button>
+
+                        </div>
+                    </form>
                 </div>
             </div>
-        )
+        </div>
+    )
 }
 
 export default ForgotPassword
